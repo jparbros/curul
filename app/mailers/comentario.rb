7c3872c13b483c0486ac4009f9a6cee6ebf9a2d7
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-11-23 09:54:47 -0600

    enviar comentario por email cuando se comenta a un diputado

--- app/mailers/comentario.rb
+++ app/mailers/comentario.rb
@@ -4,6 +4,6 @@ class Comentario < ActionMailer::Base
   def send_email(nombre, comentario, email = nil)
     to_emails = ['info@citivox.com', 'curul501@fundar.org.mx','jparbros@gmail.com']
     to_emails << email if email
-    mail(:to => [], :from => email, :subject => "Haz recibido un comentario de #{nombre}", :body => "#{nombre} comenta: \n #{comentario}")
+    mail(:to => to_emails, :subject => "Haz recibido un comentario de #{nombre}", :body => "#{nombre} comenta: \n #{comentario}")
   end
 end
