6ec9b7f1ea2fb566b0d6755ee15d252cb6de458c
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-12-12 03:18:54 -0600

    radio chekado segun el voto

--- app/controllers/vote_down_controller.rb
+++ app/controllers/vote_down_controller.rb
@@ -4,6 +4,6 @@ class VoteDownController < ApplicationController
     @iniciativa = Initiative.find(params[:iniciativa_id])
     @iniciativa.votes.vote_down
     cookies["voted_#{@iniciativa.id}"] = true
-    redirect_to request.env["HTTP_REFERER"] + '?voted=true'
+    redirect_to request.env["HTTP_REFERER"] + '?voted=true&voto=contra'
   end
 end
--- app/controllers/vote_up_controller.rb
+++ app/controllers/vote_up_controller.rb
@@ -3,6 +3,6 @@ class VoteUpController < ApplicationController
     @iniciativa = Initiative.find(params[:iniciativa_id])
     @iniciativa.votes.vote_up
     cookies["voted_#{@iniciativa.id}"] = true
-    redirect_to request.env["HTTP_REFERER"] + '?voted=true'
+    redirect_to request.env["HTTP_REFERER"] + '?voted=true&vota=favor'
   end
 end
