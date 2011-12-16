3847f8e327075e769a84904fa4d89008457894a1
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-12-16 10:28:08 -0600

    integracion de los diseños de haro

--- app/controllers/search_initiatives_controller.rb
+++ app/controllers/search_initiatives_controller.rb
@@ -1,7 +1,7 @@
 class SearchInitiativesController < ApplicationController
   
   def create
-    @query = params[:search].merge params[:date]
+    @query = params[:search].merge(params[:date] || {})
     @iniciativas = InitiativeSearch.new(@query, params[:page]).initiatives
     @temas = Topic.all
     @query = @query.delete_if {|key, value| value.blank? }
