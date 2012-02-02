ff655ccb7a5116ca3219f655b08d34fd73c62367
Jorge Pardiñas <jparbros@gmail.com>
2012-02-02 09:58:34 -0600

    representantes-comisiones fixed

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -14,20 +14,20 @@ $(document).ready(function(){
   $(".alert-message .close").click(function(){
     $(".alert-message").alert('close');
   });
-  
+
   $("#initiative_topic_tokens").tokenInput("/admin/topics.json", {
       theme: "facebook",
       prePopulate: $("#initiative_topic_tokens").data("pre"),
       crossDomain: false
   });
-  
+
   $("#initiative_presented_by_token").tokenInput("/admin/representatives.json", {
       theme: "facebook",
       prePopulate: $("#initiative_presented_by_token").data("pre"),
       crossDomain: false
   });
-  
-  $("#representative_commission_tokens").tokenInput("/admin/representatives.json", {
+
+  $("#representative_commission_tokens").tokenInput("/admin/commissions.json", {
       theme: "facebook",
       prePopulate: $("#representative_commission_tokens").data("pre"),
       crossDomain: false
--- app/controllers/admin/commissions_controller.rb
+++ app/controllers/admin/commissions_controller.rb
@@ -1,13 +1,21 @@
 class Admin::CommissionsController < Admin::BaseController
-  
+
   def index
-    @commisions = Commission.page(params[:page])
+    @commisions =   if params[:q]
+        Commission.where("name ilike ?", "%#{params[:q]}%")
+      else
+        Commission.page(params[:page])
+      end
+    respond_to do |format|
+      format.html
+      format.json { render :json => @commisions.map(&:attributes) }
+    end
   end
-  
+
   def new
     @commission = Commission.new
   end
-  
+
   def create
     @commission = Commission.new(params[:commission])
     if @commission.save
@@ -16,11 +24,11 @@ class Admin::CommissionsController < Admin::BaseController
       render :new
     end
   end
-  
+
   def edit
     @commission = Commission.find(params[:id])
   end
-  
+
   def update
     @commission = Commission.find(params[:id])
     if @commission.update_attributes(params[:commission])
@@ -29,7 +37,7 @@ class Admin::CommissionsController < Admin::BaseController
       render :edit
     end
   end
-  
+
   def destroy
     @commission = Commission.find(params[:id])
     @commission.destroy
