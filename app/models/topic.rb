f904f32018b55375503114dcbaca7d07f655d691
Jorge Pardiñas <jparbros@gmail.com>
2012-02-20 08:44:03 -0600

    temas ordenados alfabeticamente

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,14 @@
 class Topic < ActiveRecord::Base
-  
+
   has_and_belongs_to_many :initiatives
-  
+
   #
   # Validations
   #
   validates :name, :presence => true, :uniqueness => true
+
+  #
+  # Scopes
+  #
+  default_scope :order => 'name ASC'
 end
