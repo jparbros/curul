bd99cda0b50b33d973586a4d87af843a37345be9
Jorge Pardiñas <jparbros@gmail.com>
2012-03-12 18:50:04 -0600

    captcha en iniciativas

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @iniciativa = Initiative.find(params[:iniciativa_id])
-    if @iniciativa.comments.create_approved(params[:comment])
+    if verify_recaptcha && @iniciativa.comments.create_approved(params[:comment])
       redirect_to :back, :notice => "Comentario creado exitosamente"
     else
       redirect_to :back, :alert => "Comentario fallo al crearse"
