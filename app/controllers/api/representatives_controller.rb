f3c2521a470d5643a6eecaa016f0ff8310c26efa
Jorge Pardiñas <jorge.parbros@parbros.net>
2012-09-02 09:51:53 -0500

    buscando por slug en lugar de por nombre

--- app/controllers/api/representatives_controller.rb
+++ app/controllers/api/representatives_controller.rb
@@ -1,6 +1,6 @@
 class Api::RepresentativesController < ApplicationController
   def show
-    region = Region.find_by_name(params[:region_name].capitalize)
+    region = Region.find_by_slug(params[:region_name])
     representative = Representative.actual_legislature.where(district: params[:district], region_id: region.id)
     render json: (representative.present? ? representative : {error: 'No se encontro el diputado.'})
   end
